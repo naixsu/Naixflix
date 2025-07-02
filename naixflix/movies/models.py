a8c1906dc2def5d5e038027407936022e37baf2f
naixsu <plnacua02@gmail.com>
2025-07-02 18:42:13 +0800

    Initial commit

---
+++ naixflix/movies/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    video_file = models.FileField(upload_to='videos/')
+
+    def __str__(self):
+        return self.title
